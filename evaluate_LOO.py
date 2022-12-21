43ca29307a3a7fdd6685716fe58b0448b1d848ad
NicoNeureiter <nico.neureiter@gmail.com>
2022-12-21 14:08:10 +0100

    remove burnin and likelihood of NA values (constant 1) before evaluating loo()

--- evaluate_LOO.py
+++ evaluate_LOO.py
@@ -5,13 +5,23 @@ import tables
 import numpy as np
 import arviz as az
 
+BURN_IN = 100
+"""Number of samples to be dropped at the start of the MCMC chain."""
+
 # Load the data into a numpy array (shape = n_samples*n_observations)
 likelihood_table = tables.open_file('data/likelihood.h5', mode='r')
 likelihood_np = likelihood_table.root.likelihood[:]
 likelihood_table.close()
 
+# drop burn-in
+likelihood_np = likelihood_np[BURN_IN:, :]
+#
+# drop NA values
+is_na = np.all(likelihood_np == 1.0, axis=0)
+likelihood_np = likelihood_np[:, ~is_na]
+
 # arviz interprets the first dimension as chains and the second as samples, but the
-# likelihood in the file is only for one chain, i.e. dimesnions start with samples.
+# likelihood in the file is only for one chain, i.e. dimensions start with samples.
 # => Append a new dimension for chains!
 likelihood_np = likelihood_np[np.newaxis, ...]
 
